9ae38059f9afcbef6341f56a49849a834c5254ce
benjaminstokes <stokesbo@gmail.com>
2019-04-09 22:28:11 -0400

    initial commit from start.spring.io

---
+++ src/main/java/com/checkmarx/iast/cloudfoundrydemo/CloudfoundryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.checkmarx.iast.cloudfoundrydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudfoundryDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudfoundryDemoApplication.class, args);
+	}
+
+}
